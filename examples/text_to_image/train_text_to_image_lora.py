b7cbd7002f66fc7148ada34b47643d4bf85d78d1
schang03-boop <72891866+schang03-boop@users.noreply.github.com>
2024-07-25 03:24:16 +0900

    fix the issues again

--- examples/text_to_image/train_text_to_image_lora.py
+++ examples/text_to_image/train_text_to_image_lora.py
@@ -512,7 +512,7 @@ def main():
     text_encoder = CLIPTextModel.from_pretrained(
         args.pretrained_model_name_or_path, subfolder="text_encoder", revision=args.revision
     )
-    text_encoder.resize_token_embeddings(len(SPECIAL_TOKENS))
+    text_encoder.resize_token_embeddings(len(tokenizer))
     vae = AutoencoderKL.from_pretrained(
         args.pretrained_model_name_or_path, subfolder="vae", revision=args.revision, variant=args.variant
     )
